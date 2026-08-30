b17768268adca5b66da773814f5bc4f7c9ebd1fd
polipagram <bouarfakawtar68@gmail.com>
2026-08-30 23:29:06 +0100

    fixed pass handling

--- srcs/commands/pass.cpp
+++ srcs/commands/pass.cpp
@@ -2,7 +2,9 @@
 
 void passHundler(Client &c, Message &M, Server &s){
     // PASS katqbel ghir mra, khasha parameter, w khasso yqabel password dyal server.
-    if(c.passIsRecived())
+    // if(c.passIsRecived())
+    // kaw : fixit lmesage dyal registered l client hta ysali nick o user
+    if(c.isRegistered())
         s.sending_queue(c, replyCmd(462, c, "PASS"));
     else if(M.params.empty())
         s.sending_queue(c, replyCmd(461, c, "PASS"));
